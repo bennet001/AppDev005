e7eb9f51c871bea2a67c1bdee52d9e52f965b9c3
DeMilleJ <josh954@gmail.com>
2014-08-19 15:41:03 -0600

    Josh's changes
    Models/Employee and Phase goals week one doc

---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿
+
+// The namespace will be changed as we combine our code.
+
+
+namespace EmployeeControlApp.Models
+{
+	public class Employee
+	{
+		public int EmployeeID { get; private set; }
+		public string EmployeeName { get; set; }
+		public bool IsSupervisor { get; set; }
+		public string Password { get; set; }
+
+		public Employee(int employeeID, string employeeName, bool isSupervisor, string password)
+		{
+			this.EmployeeID = employeeID;
+			this.EmployeeName = employeeName;
+			this.IsSupervisor = isSupervisor;
+			this.Password = password;
+		}
+	}
+}
