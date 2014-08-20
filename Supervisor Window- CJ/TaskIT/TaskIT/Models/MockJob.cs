042694192a2b68213adf787d599f45df71fdf224
CJ-Rains <crains@student.neumont.edu>
2014-08-20 12:48:51 -0600

    Supervisor Window
    All necessary classes used in the supervisor window.  This includes the
    job popup window for creating a new job.

---
+++ Supervisor Window- CJ/TaskIT/TaskIT/Models/MockJob.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskIT.Models
+{
+    class MockJob
+    {
+        public int JobID { get; set; }
+        public string JobName { get; set; }
+        public string Description { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public string POC { get; set; }
+        public int Priority { get; set; }
+        public DateTime TimeStarted { get; set; }
+        public DateTime TimeFinished { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public MockJob(int jobId, string description, DateTime dateSubmitted, string poc, int priority, bool isCompleted)
+        {
+            JobID = jobId;
+            Description = description;
+            DateSubmitted = dateSubmitted;
+            POC = poc;
+            Priority = priority;
+            IsCompleted = isCompleted;
+        }
+    }
+}
---
+++ Supervisor Window- CJ/TaskIT/TaskIT/UserControls/CreateJobPopup.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaskIT.Delegates;
+
+namespace TaskIT.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TestUserControl.xaml
+    /// </summary>
+    public partial class CreateJobPopup : UserControl
+    {
+        public event AddDelegate JobDel;
+
+        public bool IsOpen 
+        {
+            get { return popup.IsOpen; }
+            set { popup.IsOpen = value; }
+        }
+        public int JobID { get; set; }
+        public string Description { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public string POC { get; set; }
+        public int Priority { get; set; }
+        public DateTime TimeStarted { get; set; }
+        public DateTime TimeFinished { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public CreateJobPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void saveJobButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateSubmitted = DateTime.Now;
+            JobDel.Invoke(JobID, Description, DateSubmitted, POC, Priority, IsCompleted);
+
+            popup.IsOpen = false;
+        }
+
+        private void pointOfContactTextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            POC = pointOfContactTextBox.Text;
+        }
+
+        private void priorityTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int result;
+
+            if(int.TryParse(priorityTextBox.Text, out result))
+            {
+                Priority = result;
+            }
+        }
+
+        private void JobDescriptionTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Description = JobDescriptionTextBox.Text;
+        }
+    }
+}
